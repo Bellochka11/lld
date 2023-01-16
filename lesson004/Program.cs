e897edc086d4ec307cb3fb52bc18e45a33655763
Белянкина Елизавета <Belyankina@mail.ru>
2023-01-16 16:46:32 +0300

    Четвертая задача

---
+++ lesson004/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine ("Введите число: ");
+int num1 = int.Parse(Console.ReadLine()!);
+int num2 = 1;
+while (num2 < num1)
+{
+    if (num2 % 2 == 0)
+    {
+        Console.WriteLine ($"{num2}");
+    } 
+
+    num2++;
+    
+   
+}
